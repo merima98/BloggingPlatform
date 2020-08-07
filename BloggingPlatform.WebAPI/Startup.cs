050384dbac9a6c49a012321bd8a34a8c0062ab9e
merima98 <merimaceranic98@gmail.com>
2020-08-07 15:18:01 +0200

    Adding new changes to BloggingPlatform.WebAPI

--- BloggingPlatform.WebAPI/Startup.cs
+++ BloggingPlatform.WebAPI/Startup.cs
@@ -2,23 +2,31 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using BloggingPlatform.WebAPI.Database;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
 namespace BloggingPlatform.WebAPI
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);   //to do after....
 
             services.AddControllers();
 
@@ -27,10 +35,6 @@ namespace BloggingPlatform.WebAPI
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
             });
 
-
-
-            var connection = @"Server=.;Database=BloggingPlatform;Trusted_Connection=True;ConnectRetryCount=0";
-            services.AddDbContext<BloggingPlatformContext>(options => options.UseSqlServer(connection));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,19 +45,7 @@ namespace BloggingPlatform.WebAPI
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
-
-            //app.UseEndpoints(endpoints =>
-            //{
-            //    endpoints.MapGet("/", async context =>
-            //    {
-            //        await context.Response.WriteAsync("Hello World!");
-            //    });
-            //});
-
-
-
-            //ovo dodajem
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
             app.UseSwagger();
 
             // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
@@ -64,15 +56,17 @@ namespace BloggingPlatform.WebAPI
                 c.RoutePrefix = "";
             });
 
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
             });
-            //------
-
-
-
+             
         }
     }
 }
