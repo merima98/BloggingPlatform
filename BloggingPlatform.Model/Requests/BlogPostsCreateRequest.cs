ffeca9a3585fe3e828df8a29d5cbcc0945d14f39
merima98 <merimaceranic98@gmail.com>
2020-08-09 23:28:32 +0200

    Adding BlogPostsCreateRequest

---
+++ BloggingPlatform.Model/Requests/BlogPostsCreateRequest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BloggingPlatform.Model.Requests
+{
+    public class BlogPostsCreateRequest
+    {
+        public string Slug { get; set; }
+
+        [Required(ErrorMessage = "You must enter Title!")]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "You must enter Description!")]
+        public string Description { get; set; }
+        [Required(ErrorMessage = "You must enter Body!")]
+        public string Body { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public virtual List<string> Tags { get; set; } = new List<string>();
+
+    }
+}
